376558b6ed4bf01536daa7e0841a207c153ae9b6
Daniel Valderrama <43081544+danny9mvp@users.noreply.github.com>
2026-05-16 12:08:26 -0500

    Merge pull request #36 from danny9mvp/feature/new-columns
    feat: added user details

--- app/model/user.py
+++ app/model/user.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timezone
 
-from sqlalchemy import DateTime, Integer, String
+from sqlalchemy import Boolean, DateTime, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.core.database import Base
@@ -13,9 +13,18 @@ class User(Base):
     email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     pwd_hash: Mapped[str] = mapped_column(String, nullable=False)
     username: Mapped[str] = mapped_column(String, nullable=False)
+    firstname: Mapped[str] = mapped_column(String, nullable=False, default="")
+    middlename: Mapped[str | None] = mapped_column(String, nullable=True)
+    lastname: Mapped[str] = mapped_column(String, nullable=False, default="")
+    second_lastname: Mapped[str | None] = mapped_column(String, nullable=True)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
     created_at: Mapped[datetime] = mapped_column(
         DateTime, default=lambda: datetime.now(timezone.utc)
     )
+    last_updated_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc)
+    )
+    last_login_at: Mapped[datetime | None] = mapped_column(DateTime, nullable=True)
 
     movements: Mapped[list["Movement"]] = relationship(
         back_populates="user", cascade="all, delete-orphan"
